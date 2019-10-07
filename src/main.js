82ae657f8d5bd65b2fd31521ac4bad13701cc574
CJChans <52377346+CJChans@users.noreply.github.com>
2019-10-07 11:29:56 +0800

    01-初始化项目结构

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//可以忽略，生产环境是否提示
 Vue.config.productionTip = false
-
+    // $mount把根实例挂载到指定的节点中，相当于el
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
